e8e2041ee69137420b1ee56bfe54d6f56d83385a
VictorParodi <victor.parodi.montero@gmail.com>
2019-02-26 03:22:07 -0500

    app1 commit 3: using React props system

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 6:00 PM"
+                avatar={faker.image.avatar()}
+                content="Comment text from the parent component"
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 7:00 PM"
+                avatar={faker.image.avatar()}
+                content="Comment text from the parent component"
+            />
+            <CommentDetail
+                author="John"
+                timeAgo="Today at 8:00 PM"
+                avatar={faker.image.avatar()}
+                content="Comment text from the parent component"
+            />
         </div>
     );
 }
