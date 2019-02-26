6df5447ff0ea26b40b37c43511426cded7896331
VictorParodi <victor.parodi.montero@gmail.com>
2019-02-26 04:10:44 -0500

    Solving cherry-pick error

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import { render } from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 6:00 PM"
-                avatar={faker.image.avatar()}
-                content="Comment text from the parent component"
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Today at 7:00 PM"
-                avatar={faker.image.avatar()}
-                content="Comment text from the parent component"
-            />
-            <CommentDetail
-                author="John"
-                timeAgo="Today at 8:00 PM"
-                avatar={faker.image.avatar()}
-                content="Comment text from the parent component"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 6:00 PM"
+                    avatar={faker.image.avatar()}
+                    content="Comment text from the parent component"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 7:00 PM"
+                    avatar={faker.image.avatar()}
+                    content="Comment text from the parent component"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="John"
+                    timeAgo="Today at 8:00 PM"
+                    avatar={faker.image.avatar()}
+                    content="Comment text from the parent component"
+                />
+            </ApprovalCard>
         </div>
     );
 }
